059dd8b9bac5a9cb6f855e129d68c1dfb181c394
etim <etimpaul22@gmail.com>
2023-09-10 23:06:55 +0100

    verify swap contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,9 +8,25 @@ const config: HardhatUserConfig = {
     "base-goerli": {
       url: process.env.ALCHEMY_COINBASE_ENDPOINT,
       accounts: [process.env.PRIVATE_KEY as string],
-      gasPrice: 1000000000
+      gasPrice: 1000000000,
     },
   },
+  etherscan: {
+    apiKey: {
+     "base-goerli": "PLACEHOLDER_STRING"
+    },
+    customChains: [
+      {
+        network: "base-goerli",
+        chainId: 84531,
+        urls: {
+         apiURL: "https://api-goerli.basescan.org/api",
+         browserURL: "https://goerli.basescan.org"
+        }
+      }
+    ]
+  },
+
 };
 
 export default config;
